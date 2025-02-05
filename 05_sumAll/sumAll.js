fa726d25a1571857901081c0d27465f1c24742fa
Adrian Widz <adrian.widz95@gmail.com>
2025-02-05 20:53:43 +0100

    Exerices 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR"
+    }
 
+    let sum = 0;
+    let start = a;
+    let stop = b;
+    if (a > b) {
+        start = b;
+        stop = a;
+    }
+    for (let i = start; i <= stop; i++) {
+        sum += i;
+    };
+    return sum;
 };
 
 // Do not edit below this line
