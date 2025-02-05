d63289f5dbc8940a3374699402fe4907195a6fe5
Adrian Widz <adrian.widz95@gmail.com>
2025-02-05 20:40:09 +0100

    Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    for (const element of elements) {
+        while (array.includes(element)) {
+            let index = array.indexOf(element);
+            if (index !== -1) {
+                array.splice(index, 1);
+            };
+        };
+    };
+    return array;
 };
 
 // Do not edit below this line
