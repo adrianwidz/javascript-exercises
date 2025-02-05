7b1167c22bb76ccb6c50fa640719e61a9f8cf5a4
Adrian Widz <adrian.widz95@gmail.com>
2025-02-05 21:14:55 +0100

    Exercies 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  const celcius = (degrees - 32) * (5/9); 
+  return Math.round(celcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  const fahrenheir =  degrees * (9/5) + 32;
+  return Math.round(fahrenheir * 10) / 10;
 };
 
 // Do not edit below this line
